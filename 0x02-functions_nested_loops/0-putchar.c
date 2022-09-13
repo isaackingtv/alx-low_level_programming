0dae752dba7c2eb15397ea4fd7e228770f638f9b
isaackingtv <isaacblack365@gmail.com>
2022-09-13 19:37:08 +0000

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,8 +9,8 @@
 */
 
 int main(void)
-{ 
- 	 _putchar('_');
+{   
+    	_putchar('_');
 	_putchar('p');
         _putchar('u');
         _putchar('t');
@@ -18,6 +18,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
         _putchar('r');
+	_putchar('\n');
 
-	return(0);
+	return (0);
 }
