769e5c067d5839c8d86d45a5149391d0d301bdbd
isaackingtv <isaacblack365@gmail.com>
2022-09-13 00:35:39 +0000

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
 * main -print the string using printf function
 *
 * Description : using the main function
-* This code prints "with proper grammar, but the outcome is a piece of art" 
+* This program prints - "with proper grammar, but the outcome is a piece of art" 
 * return :0
 */
 int main(void)
