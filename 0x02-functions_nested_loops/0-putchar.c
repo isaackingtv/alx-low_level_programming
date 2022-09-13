11d3c18e19db9b03d9f22d3682f4447f73568b97
isaackingtv <isaacblack365@gmail.com>
2022-09-13 17:54:21 +0000

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print _putchar, followed by a new line
+ *
+ * Description: using the main function
+ * this program prints "_putchar followed by a new line"
+ * Return: 0
+ * /
+ 
+  int main(void)
+  {
+  	_putchar("_putchar,\n");
+	return(0);
+  }
