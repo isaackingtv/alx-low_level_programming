5d675bc69705422cb716c3dbc4f29396cacc19c1
isaackingtv <isaacblack365@gmail.com>
2022-09-13 19:08:24 +0000

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,23 @@
-#include <stdio.h>
+#include <main.h>
 
 /**
- * main - print _putchar, followed by a new line
+ * main - print putchar, followed by a new line
  *
  * Description: using the main function
  * this program prints "_putchar followed by a new line"
  * Return: 0
  * /
  
-  int main(void)
-  {
-  	_putchar("_putchar,\n");
+   int main(void)
+  { 
+  	_putchar('_');
+	_putchar('p');
+        _putchar('u');
+        _putchar('t');
+        _putchar('c');
+	_putchar('h');
+	_putchar('a');
+        _putchar('r');
+
 	return(0);
   }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+ #ifndef HOLBERTON_H_INCLUDED
+# ifndef HOLBERTON_H_INCLUDED
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_X10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+int add(int a,int b);
+void print_to_98(int n);
+void print_times_tables(int n);
+void num(int r, int n);
+
+#endif
