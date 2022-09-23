f4e0fb4ec947862e2f91dcd82c71c638f0475a22
isaackingtv <isaacblack365@gmail.com>
2022-09-23 06:50:44 -0700

    commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,25 @@
 #include "main.h"
 
 /**
- *  strcat - Concantenate the string pointed to bu @src, including the terminal 
+ *  _strcat -> Concantenate the string pointed to bu @src, including the terminal 
  *  	null byte, to the end of he string pointed to by @dest
  *  @dest: A pointer to the string to be concatenated upon
  *  @src: The source string to be appended to @dest.
  *
  *  Return: A pointer to the destination string @dest
  */
-char *strcat(char *dest, const char *src)
+char *_strcat(char *dest, char *src)
 {
-		int inbox = 0, dest_len = 0;
+		int len = 0, i;
 
-		while ( dest[index++])
-			dest_len++;
+		while (dest[len])
+			len++;
 
-		for (index = 0; src[index]; index++)
-			dest[dest_len++] = src [index];
-
-		return (dest);	
+		for (i = 0; src[i] != '\0'; i++)
+		{
+			dest[len++] = src [i];
+			len += 1;
+		}
+		dest[len] = '\0'
+		return (dest)
 }
